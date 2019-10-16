715e321c6bbab4b10741be00e33c55917dea213d
Astafeva Irina <astafeya99@gmail.com>
2019-10-16 22:43:09 +0300

    add tests, fix creation random matrix, fix code style

--- modules/task_1/astafeva_i_max_val_in_row/astafeva_i_max_val_in_row.cpp
+++ modules/task_1/astafeva_i_max_val_in_row/astafeva_i_max_val_in_row.cpp
@@ -6,85 +6,86 @@
 #include "astafeva_i_max_val_in_row.h"
 
 std::vector<int> getRandomMatrix(int rows, int columns) {
-	srand(time(NULL));
-	const size_t r = rows;
-	const size_t c = columns;
-	const size_t size = r * c;
-	std::vector<int> matrix(size);
-	for (size_t i = 0; i < size; i++) {
-		matrix[i] = rand() % 101;
-	}
-	return matrix;
+    if ((rows < 0) || (columns < 0)) throw - 1;
+    srand(time(NULL));
+    const size_t r = rows;
+    const size_t c = columns;
+    const size_t size = r * c;
+    std::vector<int> matrix(size);
+    for (size_t i = 0; i < size; i++) {
+        matrix[i] = rand() % 101;
+    }
+    return matrix;
 }
 
 std::vector<int> getMaxInRows(const std::vector<int> matrix, int rows, int columns) {
-	if ((rows <= 0) || (columns <= 0)) throw - 1;
-	const size_t r = rows;
-	const size_t c = columns;
-	std::vector<int> maxs(r);
-	int j = 0;
-	for (size_t i = 0; i < r * c; i += c) {
-		int maxInRow = *std::max_element(&matrix[i], &matrix[i+c]);
-		maxs[j] = maxInRow;
-		j++;
-	}
-	return maxs;
+    if ((rows <= 0) || (columns <= 0)) throw - 1;
+    const size_t r = rows;
+    const size_t c = columns;
+    std::vector<int> maxs(r);
+    int j = 0;
+    for (size_t i = 0; i < r * c; i += c) {
+        int maxInRow = *std::max_element(&matrix[i], &matrix[i+c]);
+        maxs[j] = maxInRow;
+        j++;
+    }
+    return maxs;
 }
 
 std::vector<int> getMaxInRowsPar(const std::vector<int> matrix, int rows, int columns) {
-	if ((rows <= 0) || (columns <= 0)) throw - 1;
-	int size, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if ((rows <= 0) || (columns <= 0)) throw - 1;
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	const int delta_rows = rows / size;
-	const int rem_rows = rows % size;
-	int start_row = delta_rows;
-	if (rem_rows != 0) {
-		start_row++;
-	}
+    const int delta_rows = rows / size;
+    const int rem_rows = rows % size;
+    int start_row = delta_rows;
+    if (rem_rows != 0) {
+        start_row++;
+    }
 
-	if ((rank == 0)&&(size > 1)) {
-		for (int proc = 1; proc < size; proc++) {
-			if (proc < rem_rows) {
-				MPI_Send(&matrix[start_row * columns], (delta_rows + 1) * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
-				start_row += delta_rows;
-				start_row++;
-			}
-			else {
-				MPI_Send(&matrix[start_row * columns], delta_rows * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
-				start_row += delta_rows;
-			}
-		}
-	}
+    if ((rank == 0)&&(size > 1)) {
+        for (int proc = 1; proc < size; proc++) {
+            if (proc < rem_rows) {
+                MPI_Send(&matrix[start_row * columns], (delta_rows + 1) * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                start_row += delta_rows;
+                start_row++;
+            }
+            else {
+                MPI_Send(&matrix[start_row * columns], delta_rows * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                start_row += delta_rows;
+            }
+        }
+    }
 
-	std::vector<int> local;
-	int local_rows;
-	if (rank == 0) {
-		if (rem_rows != 0) {
-			local_rows = delta_rows + 1;
-		}
-		else {
-			local_rows = delta_rows;
-		}
-		local.resize(local_rows * columns);
-		local = std::vector<int>(&matrix[0], &matrix[0] + local_rows * columns);
-	}
-	else {
-		if (rank < rem_rows) {
-			local_rows = delta_rows + 1;
-		}
-		else {
-			local_rows = delta_rows;
-		}
-		local.resize(local_rows * columns);
-		MPI_Status status;
-		MPI_Recv(&local[0], local_rows * columns, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	}
+    std::vector<int> local;
+    int local_rows;
+    if (rank == 0) {
+        if (rem_rows != 0) {
+            local_rows = delta_rows + 1;
+        }
+        else {
+            local_rows = delta_rows;
+        }
+        local.resize(local_rows * columns);
+        local = std::vector<int>(&matrix[0], &matrix[0] + local_rows * columns);
+    }
+    else {
+        if (rank < rem_rows) {
+            local_rows = delta_rows + 1;
+        }
+        else {
+            local_rows = delta_rows;
+        }
+        local.resize(local_rows * columns);
+        MPI_Status status;
+        MPI_Recv(&local[0], local_rows * columns, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    }
 
-	std::vector<int> global_result(rows);
-	std::vector<int> local_result = getMaxInRows(local, local_rows, columns);
+    std::vector<int> global_result(rows);
+    std::vector<int> local_result = getMaxInRows(local, local_rows, columns);
 
-	MPI_Gather(&local_result[0], local_rows, MPI_INT, &global_result[0], local_rows, MPI_INT, 0, MPI_COMM_WORLD);
-	return global_result;
+    MPI_Gather(&local_result[0], local_rows, MPI_INT, &global_result[0], local_rows, MPI_INT, 0, MPI_COMM_WORLD);
+    return global_result;
 }
--- modules/task_1/astafeva_i_max_val_in_row/main.cpp
+++ modules/task_1/astafeva_i_max_val_in_row/main.cpp
@@ -1,44 +1,75 @@
 // Copyright 2019 Astafeva Irina
 
-#include <iostream>
-#include <cstdlib>
-#include <gtest-mpi-listener.hpp>
-#include "astafeva_i_max_val_in_row.h"
+#include <gtest-mpi-listener.hpp> 
+#include <gtest/gtest.h> 
+#include "astafeva_i_max_val_in_row.h" 
+
+TEST(Get_Random_Matrix, Cant_Create_Matrix_With_Negative_Parametrs) {
+    ASSERT_ANY_THROW(std::vector<int> matrix = getRandomMatrix(-2, 10));
+    ASSERT_ANY_THROW(std::vector<int> matrix = getRandomMatrix(5, -8));
+}
+
+TEST(Get_Max_In_Rows_Par, Rows_Aliquot_Size) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rows = 2 * size;
+    int columns = 5;
+
+    std::vector<int> matrix = getRandomMatrix(rows, columns);
+
+    std::vector<int> result1 = getMaxInRowsPar(matrix, rows, columns);
+
+    if (rank == 0) {
+        std::vector<int> result2 = getMaxInRows(matrix, rows, columns);
+        ASSERT_EQ(result1, result2);
+    }
+}
+
+TEST(Get_Max_In_Rows_Par, Rows_Not_Aliquot_Size) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rows = 2 * size + size / 2;
+    int columns = 5;
+
+    std::vector<int> matrix = getRandomMatrix(rows, columns);
+
+    std::vector<int> result1 = getMaxInRowsPar(matrix, rows, columns);
+
+    if (rank == 0) {
+        std::vector<int> result2 = getMaxInRows(matrix, rows, columns);
+        ASSERT_EQ(result1, result2);
+    }
+}
+
+TEST(Get_Max_In_Rows_Par, Cant_Work_With_Empty_Matrix) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rows = 0;
+    int columns = 0;
+
+    std::vector<int> matrix = getRandomMatrix(0, 0);
+
+    ASSERT_ANY_THROW(std::vector<int> result1 = getMaxInRowsPar(matrix, rows, columns));
+
+    if (rank == 0) {
+        ASSERT_ANY_THROW(std::vector<int> result2 = getMaxInRows(matrix, rows, columns));
+    }
+}
 
 int main(int argc, char** argv) {
-	int r = 6;
-	int c = 6;
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	std::vector<int> v = getRandomMatrix(r, c);
-	std::vector<int> max = getMaxInRows(v, r, c);
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-	MPI_Init(&argc, &argv);
-	int rank, size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	std::vector<int> maxPar = getMaxInRowsPar(v, r, c);
-
-	MPI_Finalize();
-	if (rank == 0) {
-		std::cout << size << std::endl << std::endl;
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				std::cout << v[i * c + j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		std::cout << std::endl;
-
-		for (int i = 0; i < r; i++)
-			std::cout << max[i] << " ";
-		std::cout << std::endl;
-		std::cout << std::endl;
-
-		for (int i = 0; i < r; i++)
-			std::cout << max[i] << " ";
-		std::cout << std::endl;
-		std::cout << std::endl;
-	}
-	return 0;
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
