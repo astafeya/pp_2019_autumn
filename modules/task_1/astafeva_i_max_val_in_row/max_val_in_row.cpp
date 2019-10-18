83b5964323bbea1e8b85f26d12490c374eeb6877
Astafeva Irina <astafeya99@gmail.com>
2019-10-19 00:13:37 +0300

    fix parallel processing 2

--- modules/task_1/astafeva_i_max_val_in_row/max_val_in_row.cpp
+++ modules/task_1/astafeva_i_max_val_in_row/max_val_in_row.cpp
@@ -45,39 +45,52 @@ std::vector<int> getMaxInRowsPar(const std::vector<int> matrix, int rows, int co
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    size_t rows_t = rows;
+    size_t columns_t = columns;
+    size_t s_t = size;
+    std::vector<int> recvcounts(s_t);
+    std::vector<int> displs(s_t);
     int s = size;
     if (size > rows) {
         s = rows;
+        s_t = rows;
     }
     const int delta_rows = rows / s;
     const int rem_rows = rows % s;
     int start_row = delta_rows;
+    size_t start_row_t = delta_rows;
     if (rem_rows != 0) {
         start_row++;
+        start_row_t++;
     }
-    std::vector<int> recvcounts(size);
-    std::vector<int> displs(size);
     recvcounts[0] = start_row;
     displs[0] = 0;
     if ((rank == 0)&&(size > 1)) {
+        size_t proc_t = 1;
         for (int proc = 1; proc < s; proc++) {
             if (proc < rem_rows) {
-                MPI_Send(&matrix[start_row * columns], (delta_rows + 1) * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
-                recvcounts[proc] = delta_rows + 1;
-                displs[proc] = start_row;
+                MPI_Send(&matrix[start_row_t * columns_t], (delta_rows + 1) * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                recvcounts[proc_t] = delta_rows + 1;
+                displs[proc_t] = start_row;
+                proc_t++;
                 start_row += delta_rows;
                 start_row++;
+                start_row_t = start_row;
             } else {
-                MPI_Send(&matrix[start_row * columns], delta_rows * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
-                recvcounts[proc] = delta_rows;
-                displs[proc] = start_row;
+                MPI_Send(&matrix[start_row_t * columns_t], delta_rows * columns, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                recvcounts[proc_t] = delta_rows;
+                displs[proc_t] = start_row;
+                proc_t++;
                 start_row += delta_rows;
+                start_row_t = start_row;
             }
         }
         if (s != size) {
+            size_t proc_t = s_t;
             for (int proc = s; proc < size; proc++) {
-                recvcounts[proc] = 0;
-                displs[proc] = start_row - 1;
+                recvcounts[proc_t] = 0;
+                displs[proc_t] = start_row - 1;
+                proc_t++;
             }
         }
     }
@@ -90,7 +103,8 @@ std::vector<int> getMaxInRowsPar(const std::vector<int> matrix, int rows, int co
         } else {
             local_rows = delta_rows;
         }
-        local.resize(local_rows * columns);
+        size_t r_t = local_rows * columns;
+        local.resize(r_t);
         local = std::vector<int>(&matrix[0], &matrix[0] + local_rows * columns);
     } else {
         if (rank < rem_rows) {
@@ -98,14 +112,14 @@ std::vector<int> getMaxInRowsPar(const std::vector<int> matrix, int rows, int co
         } else {
             local_rows = delta_rows;
         }
-        local.resize(local_rows * columns);
+        size_t r_t = local_rows * columns;
+        local.resize(r_t);
         if (rank < s) {
             MPI_Status status;
             MPI_Recv(&local[0], local_rows * columns, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         }
     }
-
-    std::vector<int> global_result(rows);
+    std::vector<int> global_result(rows_t);
     std::vector<int> local_result = getMaxInRows(local, local_rows, columns);
 
     MPI_Gatherv(&local_result[0], local_rows, MPI_INT, &global_result[0],
